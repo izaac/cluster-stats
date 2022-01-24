391a9e5df977f7c0968c163d390fc72ff815481c
Izaac Zavaleta <izaac@rancher.com>
2022-01-24 13:14:30 -0700

    mod2 checks

--- v2/clusterstats.go
+++ v2/clusterstats.go
@@ -1,11 +1,9 @@
-package main
+package clusterstats
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"io/ioutil"
-	"path/filepath"
 	"sort"
 	"strings"
 
@@ -13,7 +11,6 @@ import (
 	apiv1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 type conf struct {
@@ -154,66 +151,4 @@ func getConfigMapStats(client *kubernetes.Clientset, ctx *context.Context, opts 
 		}
 	}
 	return largestCMSize, largestCMName, largestCMNamespace, mapOfLargestCMs, err
-}
-
-func main() {
-	var kubeconfig *string
-	var c conf
-	configPath := c.getConf().Kubeconfigpath
-	configNames, err := getKubeConfigsList(configPath)
-	if err != nil {
-		panic(err)
-	}
-
-	for i, configName := range configNames {
-		fmt.Println(filepath.Join(configPath, configName))
-		kubeconfig = flag.String(fmt.Sprintf("kubeconfig%d", i), filepath.Join(configPath, configName), "absolute path to kubeconfig")
-		flag.Parse()
-		config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-		if err != nil {
-			panic(err)
-		}
-		clientset, err := kubernetes.NewForConfig(config)
-		if err != nil {
-			panic(err)
-		}
-		options := v1.ListOptions{}
-		ctx := context.TODO()
-		namespaceList, err := clientset.CoreV1().Namespaces().List(ctx, options)
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-
-		fmt.Println()
-		totalCRBs, err := getTotalCRBs(clientset, &ctx, &options)
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-		fmt.Printf("- ClusterRoleBindings Total: %d\n", totalCRBs)
-
-		totalSecrets, err := getSecretsStats(clientset, &ctx, &options, namespaceList)
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-		fmt.Printf("- Secrets Total: %d\n", totalSecrets)
-
-		roleBindingsTotal, err := getTotalRBs(clientset, &ctx, &options, namespaceList)
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-		fmt.Printf("- RoleBindings Total: %d\n", roleBindingsTotal)
-
-		largestCMSize, largestCMName, largestCMNamespace, mapOfLargestCMs, err := getConfigMapStats(clientset, &ctx, &options, namespaceList)
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-		fmt.Printf("- Largest ConfigMap Size: %d\n", largestCMSize)
-		fmt.Printf("- Largest ConfigMap Name: %s\n", largestCMName)
-		fmt.Printf("- Namespace with largest ConfigMap : %s\n", largestCMNamespace)
-		fmt.Println()
-		fmt.Println("=== ConfigMaps Name and Size ===")
-		printMap(mapOfLargestCMs)
-		kubeconfig = nil
-		fmt.Println()
-	}
 }
